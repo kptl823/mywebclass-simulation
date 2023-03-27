b200df2e4f9c2bd85483c6fd20ccc1f0cd6ab4b3
kptl823 <123340945+kptl823@users.noreply.github.com>
2023-03-27 19:28:29 -0400

    Update mywebclass_gdpr.spec.js

--- tests/mywebclass_gdpr.spec.js
+++ tests/mywebclass_gdpr.spec.js
@@ -1,4 +1,11 @@
 const { test, expect } = require('@playwright/test');
-test('GDPR Consent Cookies are stored', async ({ page, context }) => {
-// Navigate to the page and agree to the GDPR policy  await page.goto('https://github.com/kptl823/mywebclass-simulation/blob/master/src/index.html');  await page.getByLabel('I agree to the GDPR Policy and Google Analytics policy.').check();  await page.waitForTimeout(1000); // Wait for 1 seconds  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-// Check if the cookie is present  const cookies = await context.cookies();  const myCookie = cookies.find(cookie => cookie.name === '_ga');  expect(myCookie).toBeTruthy();});
+test('GDPR Cookies are stored', async ({ page, context }) => {  
+
+
+await page.goto('https://github.com/kptl823/mywebclass-simulation/blob/master/src/index.html');  
+await page.getByLabel('I agree to the GDPR Policy and Google Analytics policy.').check();  
+await page.waitForTimeout(1000); //  for 1 seconds  
+await page.getByRole('button', { name: 'Agree', exact: true }).click();
+ const cookies = await context.cookies();  
+ const myCookie = cookies.find(cookie => cookie.name === '_ga');  
+ expect(myCookie).toBeTruthy();});
