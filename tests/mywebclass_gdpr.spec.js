6879f23d9581e55cb1d108c7241cebf93e7b61fc
kptl823 <123340945+kptl823@users.noreply.github.com>
2023-03-27 19:34:06 -0400

    Update mywebclass_gdpr.spec.js

--- tests/mywebclass_gdpr.spec.js
+++ tests/mywebclass_gdpr.spec.js
@@ -3,7 +3,7 @@ test('GDPR Cookies are stored', async ({ page, context }) => {
 
 
 await page.goto('https://github.com/kptl823/mywebclass-simulation/blob/master/src/index.html');  
-await page.getByLabel('I agree to the GDPR Policy and Google Analytics policy.').check();  
+await page.getByLabel('I agree to the GDPR and Google Analytics policy.').check();  
 await page.waitForTimeout(1000); //  for 1 seconds  
 await page.getByRole('button', { name: 'Agree', exact: true }).click();
  const cookies = await context.cookies();  
