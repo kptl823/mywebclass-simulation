94a522d2bfaf663ae661ca3c4e65af3858b31a24
shivam patil <shivpatil230@gmail.com>
2023-03-27 20:04:38 -0400

    update  file mywebclass_gdpr.spec.js

--- tests/mywebclass_gdpr.spec.js
+++ tests/mywebclass_gdpr.spec.js
@@ -1,22 +1,71 @@
-// Check if user has given consent for cookies
-function checkConsent() {
-  return localStorage.getItem('cookieConsent');
+// ga.js - Google Analytics tracking code
+
+function loadGA() {
+  // Load Google Analytics script
+  var gaScript = document.createElement('script');
+  gaScript.setAttribute('async', '');
+  gaScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID');
+  document.head.appendChild(gaScript);
+
+  // Initialize Google Analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'GA_MEASUREMENT_ID', {
+    'anonymize_ip': true, // Enable IP anonymization
+    'cookie_expires': 31536000, // Set data retention period to 1 year
+    'cookie_prefix': 'ga', // Customize cookie name prefix
+    'allow_google_signals': false, // Disable advertising features
+    'send_page_view': false // Manually trigger pageview events
+  });
 }
 
-// Set consent for cookies
-function setConsent() {
-  localStorage.setItem('cookieConsent', 'true');
+function initGA() {
+  // Check if user has already given consent
+  var consentCookie = getCookie('ga_consent');
+  if (consentCookie == 'true') {
+    loadGA();
+  } else {
+    // Show confirmation modal for user consent
+    showConsentModal();
+  }
 }
 
-// Show cookie consent banner if consent has not been given
-if (!checkConsent()) {
-  const banner = document.createElement('div');
-  banner.innerHTML = `
-    <div class="cookie-banner">
-      <p>This website uses cookies to improve your experience. Click "Accept" to consent to the use of cookies.</p>
-      <button class="cookie-accept" onclick="setConsent()">Accept</button>
-    </div>
-  `;
-  document.body.appendChild(banner);
+function getCookie(name) {
+  // Helper function to get cookie value by name
+  var value = '; ' + document.cookie;
+  var parts = value.split('; ' + name + '=');
+  if (parts.length == 2) {
+    return parts.pop().split(';').shift();
+  }
 }
 
+function setCookie(name, value, days) {
+  // Helper function to set cookie value with expiration date
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+function showConsentModal() {
+  // Show confirmation modal for user consent
+  // ...
+  // Handle user clicks on "Accept" or "Decline" buttons
+  var acceptButton = document.getElementById('accept-button');
+  acceptButton.addEventListener('click', function() {
+    setCookie('ga_consent', 'true', 365); // Set cookie for 1 year
+    loadGA();
+    // Hide confirmation modal
+  });
+  var declineButton = document.getElementById('decline-button');
+  declineButton.addEventListener('click', function() {
+    setCookie('ga_consent', 'false', 365); // Set cookie for 1 year
+    // Hide confirmation modal
+  });
+}
+
+initGA();
