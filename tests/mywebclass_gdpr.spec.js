c7d34ae305948352befe27147f3bdbc494017e46
shivam patil <shivpatil230@gmail.com>
2023-03-27 20:00:30 -0400

    update  file mywebclass_gdpr.spec.js

--- tests/mywebclass_gdpr.spec.js
+++ tests/mywebclass_gdpr.spec.js
@@ -1,3 +1,22 @@
-const { test, expect } = require('@playwright/test');
-test('GDPR Consent Cookies are stored', async ({ page, context }) => {  // Navigate to the page and agree to the GDPR policy  await page.goto('https://github.com/kptl823/mywebclass-simulation/blob/master/src/index.html');  await page.getByLabel('I agree to the GDPR Policy and Google Analytics policy.').check();  await page.waitForTimeout(1000); // Wait for 1 seconds  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  // Check if the cookie is present  const cookies = await context.cookies();  const myCookie = cookies.find(cookie => cookie.name === '_ga');  expect(myCookie).toBeTruthy();});
+// Check if user has given consent for cookies
+function checkConsent() {
+  return localStorage.getItem('cookieConsent');
+}
+
+// Set consent for cookies
+function setConsent() {
+  localStorage.setItem('cookieConsent', 'true');
+}
+
+// Show cookie consent banner if consent has not been given
+if (!checkConsent()) {
+  const banner = document.createElement('div');
+  banner.innerHTML = `
+    <div class="cookie-banner">
+      <p>This website uses cookies to improve your experience. Click "Accept" to consent to the use of cookies.</p>
+      <button class="cookie-accept" onclick="setConsent()">Accept</button>
+    </div>
+  `;
+  document.body.appendChild(banner);
+}
+
