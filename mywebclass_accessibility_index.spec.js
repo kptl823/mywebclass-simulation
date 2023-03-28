d9725e95f96d85d908a07d6d7846752a828ce7e4
kajolp329 <kajolp329@gmail.com>
2023-03-27 21:07:31 -0400

    added web accessibility code

--- mywebclass_accessibility_index.spec.js
+++ mywebclass_accessibility_index.spec.js
@@ -4,3 +4,4 @@ test.describe('Accessibility tests for MyWebClass.org', () => {  let page;
   test.afterAll(async () => {    await page.close();  });
   test('Images have alt text', async () => {    await page.goto('http://localhost:3000/');    const imagesWithoutAltText = await page.$$eval('img:not([alt])', imgs => imgs.map(img => img.src));    expect(imagesWithoutAltText.length).toBe(0);  });
   test('Newsletter input has a label', async () => {    await page.goto('http://localhost:3000/');    const label = await page.$('label[for="newsletter1"]');    expect(label).toBeTruthy();  });});
+  const ariaCurrentAttr = await currentLink.getAttribute('aria-current');  expect(ariaCurrentAttr).toBe('page');
